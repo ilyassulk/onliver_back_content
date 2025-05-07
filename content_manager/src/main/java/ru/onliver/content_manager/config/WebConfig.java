0c50156840ad892572f1a5825625cd92684da353
ilyassulk <20044002ilya@gmail.com>
2025-05-07 20:13:56 +0300

    ia ebal etot CORS

--- content_manager/src/main/java/ru/onliver/content_manager/config/WebConfig.java
+++ content_manager/src/main/java/ru/onliver/content_manager/config/WebConfig.java
@@ -3,15 +3,15 @@ package ru.onliver.content_manager.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")                        // для всех путей
-                .allowedOrigins("http://localhost", "http://localhost:3000", "http://onliver.ru:8080", "https://onliver.ru:8080", "http://onliver.ru", "https://onliver.ru")  // доверенный origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);                 // если нужны куки/авторизация
-    }
-}
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")                        // для всех путей
+//                .allowedOrigins("http://localhost", "http://localhost:3000", "http://onliver.ru:8080", "https://onliver.ru:8080", "http://onliver.ru", "https://onliver.ru")  // доверенный origin
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);                 // если нужны куки/авторизация
+//    }
+//}
