89149a2553ec2e40971fce1a55f95b8151fae7aa
ilyassulk <20044002ilya@gmail.com>
2025-05-07 13:52:05 +0300

    Initial commit

---
+++ content_manager/src/main/java/ru/onliver/content_manager/ContentManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.onliver.content_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentManagerApplication.class, args);
+	}
+
+}
---
+++ content_manager/src/test/java/ru/onliver/content_manager/ContentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.onliver.content_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
